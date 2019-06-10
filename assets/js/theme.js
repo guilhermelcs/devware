ccb5499fb86d094a331acfd2685ac3cb63600f67
Guilherme Lucas <guilherme.evandf@gmail.com>
2019-06-10 01:48:26 -0300

    Adicionando varias seções, parallax e js

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,9 +1,10 @@
 // @Author: Davi
 
-// Parallax
-var elements = [document.getElementsByClassName( 'section-marcante' ), document.getElementsByClassName( 'section-experiencia' )];
-    for( var element of elements ) {
-        var position = element.offsetTop;
+// O VERDADEIRO Parallax
+var elements = document.getElementsByClassName( 'parallax' );
+
+    for( let element of elements ) {
+        let position = element.offsetTop;
         position = position - 500;
         if( position < 0 ) {
             position = 0;
@@ -15,3 +16,11 @@ var elements = [document.getElementsByClassName( 'section-marcante' ), document.
             // console.log( window.pageYOffset * 0.03 );
         });
     }
+
+$( 'document' ).ready(function() {
+    $('#menu-icon, .menu-cover').hover( function() {
+        $('.menu-cover, .menu-list').addClass('appear');
+    }, function(){
+        $('.menu-cover, .menu-list').removeClass('appear');
+    });
+});
